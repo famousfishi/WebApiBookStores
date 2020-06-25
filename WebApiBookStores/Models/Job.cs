811975f7b6fee349342bf46635f449433ad13b05
Fisayo Oluwayemi (Dev Fishi) <fisayo@wellahealth.com>
2020-06-25 12:43:11 +0100

    Add project files.

---
+++ WebApiBookStores/Models/Job.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApiBookStores.Models
+{
+    public partial class Job
+    {
+        public short JobId { get; set; }
+        public string JobDesc { get; set; }
+    }
+}
